ccebc25174e8e26869e617248008f0b2b352f7b6
juho <wajuan19@gmail.com>
2025-06-04 20:41:40 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# juan-kiosk
-# juan-kiosk
--- project-port/css/salimia.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let name = document.getElementById("nameInput").value;
-    document.getElementById("result").innerText = "Hello, " + name + "!\n";
-});
